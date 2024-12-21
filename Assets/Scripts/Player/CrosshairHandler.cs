7166d6b946336bde8943031a93787c35527a2ff0
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2024-12-21 13:42:07 -0300

    Clampado o crosshair na tela

--- Assets/Scripts/Player/CrosshairHandler.cs
+++ Assets/Scripts/Player/CrosshairHandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrosshairHandler : MonoBehaviour
-
 {
     public Transform player;
     public Transform crosshair;
@@ -22,15 +21,19 @@ public class CrosshairHandler : MonoBehaviour
         // Pega a posição do mouse na tela
         Vector3 mousePosTela = Input.mousePosition;
 
+        // Garante que o mouse não saia da tela
+        mousePosTela.x = Mathf.Clamp(mousePosTela.x, 0, Screen.width);
+        mousePosTela.y = Mathf.Clamp(mousePosTela.y, 0, Screen.height);
+
         // Converte a posição do mouse para o mundo
         Vector3 mousePosMundo = Camera.main.ScreenToWorldPoint(mousePosTela);
         mousePosMundo.z = 0; // Z = 0 para 2D (evita que a mira suba ou desça no eixo Z)
 
-        Vector3 direcaoMira = mousePosMundo - player.position;
-
+        // Define a posição da mira
         crosshair.position = mousePosMundo;
 
-        angle = Mathf.Atan2(direcaoMira.y, direcaoMira.x) * Mathf.Rad2Deg  - 90f;
+        // Calcula o ângulo da mira em relação ao jogador
+        Vector3 direcaoMira = mousePosMundo - player.position;
+        angle = Mathf.Atan2(direcaoMira.y, direcaoMira.x) * Mathf.Rad2Deg - 90f;
     }
-
 }
