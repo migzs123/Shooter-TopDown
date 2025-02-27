6c23f2d570a8f7c7abb692172b01b9c0d89e980e
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2025-02-27 14:29:06 -0300

    Criado o a cena do menu principal

---
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string levelName;
+
+   public void Play()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    
+
+    public void Exit()
+    {
+        Debug.Log("Sair do jogo");
+        Application.Quit();
+    }
+}
