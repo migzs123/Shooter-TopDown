860261b7675dc69769f23fd5d6c2c2d3ea8aef79
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2025-07-10 15:15:52 -0300

    Feito um feeback do hit

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,29 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private Slider slider;
 
     [SerializeField] private float maxHealth=4f;
     [SerializeField] private float health=4f;
 
     [SerializeField] private PowerUpsManager powerUpsManager;
 
-    public void TakeDamage(float damage){
+    [Header("Feedback visual")]
+    [SerializeField] private Color flashColor = Color.red;
+    [SerializeField] private float flashDuration = 0.1f;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void TakeDamage(float damage)
+    {
         if (powerUpsManager.getItem() == 2)
         {
             damage *= 2;
         }
 
-        health-= damage;
+        health -= damage;
+
+        // Piscar visual
+        StartCoroutine(Flash());
+
         if (health <= 0)
         {
             Destroy(gameObject);
         }
     }
 
+    private IEnumerator Flash()
+    {
+        Color originalColor = spriteRenderer.color;
+        spriteRenderer.color = flashColor;
+
+        yield return new WaitForSeconds(flashDuration);
+
+        spriteRenderer.color = originalColor;
+    }
 }
+
